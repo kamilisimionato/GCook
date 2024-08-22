dc1a2fdf19f2297ea7a2c59df8d6dc495f851c0b
kamilisimionato <kamili.simionato@etec.sp.gov.br>
2024-08-22 12:29:15 -0300

    Finalizando a camada de dados

--- GCook/Models/Categoria.cs
+++ GCook/Models/Categoria.cs
@@ -8,11 +8,13 @@ public class Categoria
 {
     [Key]
     public int Id { get; set; }
-    
+
     [Required]
     [StringLength(30)]
     public string Nome { get; set; }
 
     [StringLength(300)]
     public string Foto { get; set; }
+
+    public bool ExibirHome { get; set; } = false;
 }
