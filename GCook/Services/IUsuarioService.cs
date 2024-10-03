ea8ba62c3a804a5354414fc30c9cfe65bba32988
kamilisimionato <kamili.simionato@etec.sp.gov.br>
2024-10-03 12:26:51 -0300

    parou em usuarioservice, linha 71/72

--- GCook/Services/IUsuarioService.cs
+++ GCook/Services/IUsuarioService.cs
@@ -1,12 +1,73 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using GCook.Data;
+using GCook.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using GCook.Helpers;
+using GCook.Models;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
+using System.Text.Encodings.Web;
 
-namespace GCook.Services
+namespace GCook.Services;
+
+public interface IUsuarioService : IUsuarioService
 {
-    public interface IUsuarioService
-    {
-        
-    }
+        private readonly AppDbContext _contexto;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IUserStore<IdentityUser> _userStore;
+        private readonly IUserEmailStore<IdentityUser> _emailStore;
+        private readonly IWebHostEnvironment _hostEnviroment;
+        private readonly IEmailSender _emailSender;
+        private readonly ILogger<UsuarioService> _logger;
+
+
+        public UsuarioService(
+            AppDbContext contexto,
+            SignInManager<IdentityUser> signInManager,
+            UserManager<IdentityUser> userManager,
+            IHttpContextAccessor httpContextAccessor,
+            IUserStore<IdentityUser> userStore,
+            IWebHostEnvironment hostEnvironment,
+            IEmailSender emailSender,
+            ILogger<UsuarioService> logger
+        )
+        {
+            _contexto = contexto;
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _httpContextAccessor = httpContextAccessor;
+            _userStore = userStore;
+            _emailStore = (IUserEmailStore<IdentityUser>)_userStore;
+            _hostEnviroment = hostEnvironment;
+            _emailSender = emailSender;
+            _logger = logger;
+        }
+
+        public async Task<bool> ConfirmarEmail(string userId, string code)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return false;
+            }
+            code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
+            var result = await _userManager.ConfirmEmailAsync(user, code);
+            return result.Succeeded;
+        }
+
+        public async Task<UsuarioVM> GetUsuarioLogado()
+        {
+            var userId = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (userId == null)
+            {
+                return null;
+            }
+            var userAccount = await _userManager.FindByIdAsync(userId);
+            var usuario = await _contexto.Usuarios.Where(u => u.UsuarioId == userId).SingleOrDefaultAsync();
+            var perfis = string.Join(", ", await _userManager.GetRolesAsync(userAccount));
+            var admin = await _userManager.IsInRoleAsync(userAccount, "Administrador");
+        }
 }
